979b07ffb2a5a0bdb6efc261c610082a011ae471
nljones4 <32282398+nljones4@users.noreply.github.com>
2018-10-24 19:07:29 -0700

    Add files via upload

---
+++ practice-assignment-07-nljones4-master/ArrayList.java
@@ -0,0 +1,66 @@
+public class ArrayList implements List{
+	private int size;
+	private Object[] a;
+	
+	public ArrayList(){
+		size = 0;
+		a = new Object[10000];
+	}
+	
+	public void add(Object obj){
+		if(size == a.length){
+			grow_array();
+		}
+		a[size] = obj;
+		++size;
+	}
+	
+	public void add(int pos, Object obj){
+		
+		if(pos > size || pos < 0){
+			throw new IllegalArgumentException("empty");
+		}
+		else if(size == a.length){
+			grow_array();
+		}
+		for(int i = size; i > pos; i--){
+			a[i]=a[i+1];
+		}
+		a[pos] = obj;
+		size++;
+	}
+	
+	public Object get(int pos){
+		if(pos > size || pos < 0){
+			throw new IllegalArgumentException("empty");
+		}
+		return a[pos];
+	}
+	
+	public Object remove(int pos){
+		if(pos >= size || pos < 0){
+			throw new IllegalArgumentException("empty");
+		}
+		Object temp = a[pos];
+		for(int i = pos; i < size-1; i++){
+			a[i] = a[i+1];
+		}
+		size--;
+		return temp;
+	}
+	
+	public int size(){
+		return size;
+	}
+	
+	public void grow_array(){
+		Object[] newArray = new Object[a.length*2];
+		for(int i = 0; i <= size; i++){
+			newArray[i] = a[i+1];
+		}
+		a = newArray;
+		
+	}
+
+
+}
